8853500787b7816e28a2c8f2a115dd1e16203043
shmsacr <shmsacr@gmail.com>
2023-01-26 14:44:26 +0300

    feat: complete projece

--- lib/controller/controller.dart
+++ lib/controller/controller.dart
@@ -4,8 +4,10 @@ import '../model/model.dart';
 import '../service/service.dart';
 
 class Controller extends ChangeNotifier{
+  PageController pageController = PageController(initialPage:0);
   bool? isLoading;
   List<Data> users = [];
+  List<Data> saved = [];
 
   void getData(){
     Service.fetch().then((value){
@@ -19,5 +21,15 @@ class Controller extends ChangeNotifier{
       }
     });
   }
-
+  void addSaved(Data model){
+    saved.add(model);
+    users.remove(model);
+    notifyListeners();
+  }
+  notSavedButton(){
+    pageController.animateToPage(0, duration: Duration(microseconds: 500), curve: Curves.easeInExpo);
+  }
+  SavedButton(){
+    pageController.animateToPage(1, duration: Duration(microseconds: 500), curve: Curves.easeInExpo);
+  }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -19,54 +19,142 @@ class _HomePageState extends ConsumerState<HomePage> {
     ref.read(controller).getData();
     super.initState();
   }
+
   Widget build(BuildContext context) {
     var read = ref.read(controller);
     var watch = ref.watch(controller);
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Riverpod",
+          style: TextStyle(color: Colors.orange),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 1,
+      ),
       body: LoadingWidget(
         isLoading: watch.isLoading,
-        child: Column(
-          children: [
-            const Text("Users"),
-            ListView.separated(
-              shrinkWrap: true,
-              itemCount: watch.users.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  shape: RoundedRectangleBorder(borderRadius: 15.allBR),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(watch.users[index].avatar!),
-                      radius: 20,
-                    ),
-                    title:Text(
-                      "${watch.users[index].firstName ?? " "} ${watch.users[index].lastName ?? " "}",
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 16,
-                      ),
-                    ),
-                    subtitle: Text(
-                      "${watch.users[index].email ?? ""}",
-                      style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          fontSize: 16,
-                          color: Colors.grey.shade50
-                      ),
+        child: Padding(
+
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 6,
+                    child: OutlinedButton(
+                      onPressed: () => read.notSavedButton(),
+                      child: Text("Kullanıcılar (${watch.users.length})"),
                     ),
                   ),
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) {
-                return const SizedBox(
-                  height: 15,
-                );
-              },
-            ),
-          ],
+                  Spacer(),
+                  Expanded(
+                    flex: 6,
+                    child: OutlinedButton(
+                      onPressed: () => read.SavedButton(),
+                      child: Text("Kaydedilenler (${watch.saved.length})"),
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: PageView(
+                  controller: watch.pageController,
+                  children: [
+                    notSaved(watch),
+                    Saved(watch),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
-}
 
+  ListView Saved(Controller watch) {
+    return ListView.separated(
+      shrinkWrap: true,
+      itemCount: watch.saved.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          shape: RoundedRectangleBorder(borderRadius: 15.allBR),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(watch.saved[index].avatar!),
+              radius: 20,
+            ),
+            title: Text(
+              "${watch.saved[index].firstName ?? " "} ${watch.saved[index].lastName ?? " "}",
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
+              ),
+            ),
+            subtitle: Text(
+              "${watch.saved[index].email ?? ""}",
+              style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
+                  color: Colors.black12),
+            ),
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return const SizedBox(
+          height: 15,
+        );
+      },
+    );
+  }
+
+  ListView notSaved(Controller watch) {
+    return ListView.separated(
+      shrinkWrap: true,
+      itemCount: watch.users.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          shape: RoundedRectangleBorder(borderRadius: 15.allBR),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(watch.users[index].avatar!),
+              radius: 20,
+            ),
+            title: Text(
+              "${watch.users[index].firstName ?? " "} ${watch.users[index].lastName ?? " "}",
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
+              ),
+            ),
+            subtitle: Text(
+              "${watch.users[index].email ?? ""}",
+              style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
+                  color: Colors.black12),
+            ),
+            trailing: IconButton(
+                onPressed: () => watch.addSaved(watch.users[index]),
+                icon: Icon(
+                  Icons.add,
+                  color: Colors.black,
+                )),
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return const SizedBox(
+          height: 15,
+        );
+      },
+    );
+  }
+}
